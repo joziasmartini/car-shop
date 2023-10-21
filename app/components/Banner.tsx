6180ce0e60a25e198f213a996f2dd054b1fcbaa0
Jozias Martini <jozias.martini@gmail.com>
2023-10-21 12:51:05 -0300

    Create website structure (#2)
    * feat: install Three.js
    
    * build: install Three.js types
    
    * feat: working with Three.js
    
    * feat: create banner, header, car list and footer
    
    * feat: update texts and designs of components

---
+++ app/components/Banner.tsx
@@ -0,0 +1,31 @@
+import { XMarkIcon } from "@heroicons/react/20/solid";
+
+export default function Banner() {
+  return (
+    <div className="flex items-center gap-x-6 bg-red-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+      <p className="text-sm leading-6 text-white">
+        <a href="#">
+          <strong className="font-semibold">CarContest 2023</strong>
+          <svg
+            viewBox="0 0 2 2"
+            className="mx-2 inline h-0.5 w-0.5 fill-current"
+            aria-hidden="true"
+          >
+            <circle cx={1} cy={1} r={1} />
+          </svg>
+          Participe conosco no evento em Denver de Junho 7 – 9&nbsp;
+          <span aria-hidden="true">&rarr;</span>
+        </a>
+      </p>
+      <div className="flex flex-1 justify-end">
+        <button
+          type="button"
+          className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+        >
+          <span className="sr-only">Dismiss</span>
+          <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
+        </button>
+      </div>
+    </div>
+  );
+}
