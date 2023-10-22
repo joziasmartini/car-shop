c89a59fe200467d86d6e69f35b67cb829e52b731
Jozias Martini <jozias.martini@gmail.com>
2023-10-22 14:41:57 -0300

    feat: working scene with cubes, now trying with 3d model

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,57 @@
+"use client";
+
+import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import CarList from "./components/CarList";
 import Footer from "./components/Footer";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const loader = new GLTFLoader();
-  let scene: any;
+  const sceneRef = useRef<HTMLDivElement | null>(null);
 
-  loader.load(
-    // resource URL
-    "3d/free_1972_datsun_240k_gt/scene.gltf",
-    // called when the resource is loaded
-    function (gltf) {
-      scene.add(gltf.scene);
+  useEffect(() => {
+    if (sceneRef.current) {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      camera.position.z = 5;
+      const renderer = new THREE.WebGLRenderer({ alpha: true });
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      scene.background = null; // or use 0x00000000
 
-      gltf.animations; // Array<THREE.AnimationClip>
-      gltf.scene; // THREE.Group
-      gltf.scenes; // Array<THREE.Group>
-      gltf.cameras; // Array<THREE.Camera>
-      gltf.asset; // Object
-    },
-    // called while loading is progressing
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    // called when loading has errors
-    function (error) {
-      console.log("An error happened: ", error);
+      const loader = new GLTFLoader(); // Use the GLTFLoader from 'three-gltf-loader'
+
+      // Load your 3D model
+      loader.load("3d/free_1972_datsun_240k_gt/scene.gltf", (gltf) => {
+        const model = gltf.scene;
+        scene.add(model);
+      });
+
+      sceneRef.current?.appendChild(renderer.domElement);
+
+      const animate = () => {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      };
+
+      animate();
+
+      return () => {
+        renderer.dispose();
+      };
     }
-  );
+  }, []);
 
   return (
     <main>
+      {/* Include absolute in className below */}
+      <div ref={sceneRef} className="z-20" />
       <Banner />
       <Header />
       <CarList />
