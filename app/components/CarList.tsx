2b192089c97092811c82b5582aee9d9e641a4b5b
Jozias Martini <jozias.martini@gmail.com>
2023-12-03 19:56:20 -0300

    Rendering models with Three.js (#3)
    * feat: install Three.js
    
    * build: install Three.js types
    
    * feat: working with Three.js
    
    * feat: temporarily fix ids of car list
    
    * feat: working scene with cubes, now trying with 3d model
    
    * feat: working rendered model
    
    * feat: render car in header
    
    * feat: improve car list design
    
    * feat: make car rotation
    
    * feat: remove unused comment

--- app/components/CarList.tsx
+++ app/components/CarList.tsx
@@ -2,6 +2,7 @@ import {
   BanknotesIcon,
   InformationCircleIcon,
 } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 const cars = [
   {
@@ -110,13 +111,15 @@ export default function CarList() {
     >
       {cars.map((car) => (
         <li
-          key={car.email}
-          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
+          key={car.name + Math.random()}
+          className="col-span-1 flex flex-col bg-white text-center rounded-md"
         >
           <div className="flex flex-1 flex-col p-8">
-            <img
+            <Image
               className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
               src={car.imageUrl}
+              width={1000}
+              height={1000}
               alt=""
             />
             <h3 className="mt-6 text-sm font-medium text-gray-900">
@@ -127,21 +130,21 @@ export default function CarList() {
               <dd className="text-sm text-gray-500">{car.title}</dd>
               <dt className="sr-only">Role</dt>
               <dd className="mt-3">
-                <span className="inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">
+                <span className="inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-600 ring-1 ring-inset ring-red-600/20">
                   {car.brand}
                 </span>
               </dd>
             </dl>
           </div>
           <div>
-            <div className="-mt-px flex divide-x divide-gray-200">
+            <div className="-mt-px flex bg-red-600 rounded-b-md">
               <div className="flex w-0 flex-1">
                 <a
                   href={`mailto:${car.email}`}
-                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-white"
                 >
                   <InformationCircleIcon
-                    className="h-5 w-5 text-gray-400"
+                    className="h-5 w-5 text-white"
                     aria-hidden="true"
                   />
                   Conhecer
@@ -150,10 +153,10 @@ export default function CarList() {
               <div className="-ml-px flex w-0 flex-1">
                 <a
                   href={`tel:${car.telephone}`}
-                  className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                  className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-white"
                 >
                   <BanknotesIcon
-                    className="h-5 w-5 text-gray-400"
+                    className="h-5 w-5 text-white"
                     aria-hidden="true"
                   />
                   Adquirir
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,23 @@
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+"use client";
+
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import CarList from "./components/CarList";
 import Footer from "./components/Footer";
+import { useEffect, useRef } from "react";
+import { initThreeScene } from "./components/ThreeScene";
 
 export default function Home() {
-  const loader = new GLTFLoader();
-  let scene: any;
+  const sceneRef = useRef<HTMLDivElement | null>(null);
 
-  loader.load(
-    // resource URL
-    "3d/free_1972_datsun_240k_gt/scene.gltf",
-    // called when the resource is loaded
-    function (gltf) {
-      scene.add(gltf.scene);
-
-      gltf.animations; // Array<THREE.AnimationClip>
-      gltf.scene; // THREE.Group
-      gltf.scenes; // Array<THREE.Group>
-      gltf.cameras; // Array<THREE.Camera>
-      gltf.asset; // Object
-    },
-    // called while loading is progressing
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    // called when loading has errors
-    function (error) {
-      console.log("An error happened: ", error);
-    }
-  );
+  useEffect(() => {
+    initThreeScene(sceneRef);
+  }, []);
 
   return (
     <main>
+      {/* Include absolute in className below */}
+      <div ref={sceneRef} className="z-20 absolute" />
       <Banner />
       <Header />
       <CarList />
